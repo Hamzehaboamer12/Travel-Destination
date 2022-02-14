3f40e97b28a81827a8cb803c8a269cea6410a78d
Hamzehaboamer12 <hamzehaboamee@gmail.com>
2022-02-15 01:53:24 +0200

    add class17 barnch

--- travel/src/components/Tours/Tours.js
+++ travel/src/components/Tours/Tours.js
@@ -4,13 +4,14 @@ function Tours(props) {
 
     return (
         <>
-            {data.map(travel => {
+            {data.map((travel,index) => {
                 return (
-                    <div>
-                        <img src={travel.image} alt={travel.name} />
-                        <h3>Name : {travel.name}</h3>
-                    </div>
-                );
+                    // <div>
+                    //     <img src={travel.image} alt={travel.name} />
+                    //     <h3>Name : {travel.name}</h3>
+                    // </div>
+                  <Tour key={index} />
+                    );
             })}
         </>
     );
